d67e2b624a0b5268406281b330d56e5ba0359ab3
rayamigo18 <rayamigo18@gmail.com>
2022-01-15 21:39:23 +1100

    fix format issue

--- src/components/pages/Catherine.js
+++ src/components/pages/Catherine.js
@@ -3,9 +3,8 @@ import "../../App.css";
 
 function Catherine() {
   return (
-    <div className="profile-container">
-      <h1 className="profile">Catherine Yap</h1>
-      <div className="profile-info">
+    <div className="aboutus-container">
+      <h1 className="about-us">Catherine Yap</h1>
         <div class="aboutus-info">
           <div class="row">
             <div class="column">
@@ -18,30 +17,27 @@ function Catherine() {
               <br></br>
               <br></br>
               <br></br>
-              <row><strong>2016: Certificate IV in Community Languages Teaching</strong><br></br>
+              <strong>2016: Certificate IV in Community Languages Teaching</strong><br></br>
               Community Languages Victoria<br></br>
               (Australian Federation of Ethnic Schools Associations)<br></br>
-              Co AS IT (Italian Centre), Carlton</row>
-              <br></br>
-              <row><strong>2012: Graduate Diploma in Secondary Education</strong><br></br>
+              Co AS IT (Italian Centre), Carlton
+              <br></br><br></br>
+              <strong>2012: Graduate Diploma in Secondary Education</strong><br></br>
               Victoria University, Melbourne<br></br>
               Subject Methods: Science/Maths<br></br>
-              </row>
+              
               <br></br>
-              <row><strong>2011: Postgraduate Diploma in Biomedical Science</strong><br></br>
+              <strong>2011: Postgraduate Diploma in Biomedical Science</strong><br></br>
               The Ritchie Centre<br></br>
               Monash Institute of Medical Research<br></br>
               Monash University, Clayton<br></br>
               Research Project: The Role of Tregs in Human <br></br>
               Amnion Epithelial Cell Mediated Lung Repair<br></br>
-              </row>
               <br></br>
-              <row><strong>2010: Bachelor of Science</strong><br></br>
+              <strong>2010: Bachelor of Science</strong><br></br>
               Monash University, Clayton Campus<br></br>
               Major: Genetics and Human Pathology<br></br>
-              </row>
             </div>  
-            </div>
           </div>
         </div>
       </div>
