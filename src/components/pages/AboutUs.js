d99f2e96f212245fc58bb1fbd1f9a42bd628a118
rayamigo18 <Amabelle Dormer>
2026-01-26 20:04:26 +1100

    Changes to text

--- src/components/pages/AboutUs.js
+++ src/components/pages/AboutUs.js
@@ -7,13 +7,30 @@ function AboutUs() {
       <h1 className="about-us">About Us</h1>
       <div className="aboutus-info">
         <p>
-        The Philippine Language School of Victoria was established and founded by Perla Leutic on 28 February 2016  and granted a Certificate of  Incorporation
-
-No. A0094860V to enable the Filipino children learn and understand the language and culture.
+       The Philippine Language School of Victoria (PLSV) was founded 1995 by Perla Adaptar Luetic, the President of LUZVIMINDA Association of Filipino Women in Victoria, Inc.
+       to enable the Filipino Children learn and understand the language and culture. The work began in cooperation with the Filipino Community Council of Victoria 
+       and Ethnic School Association of Victoria. The school was designed for non-speakers of Filipino Language, or person of Filipino parents who are Filipino-Australian 
+       born and who do not speak the language, children of mixed marriages, scholars, diplomats, businesspeople and individuals who are interested to learn the Filipino
+       language.
         </p>
-        <p>The work began in 1994 in co-operation with the Filipino Community Council of Victoria and Ethnic School Association of Victoria, Inc.</p>
-        <p>The School is designed for non-speakers of Filipino language, or persons of Filipino parents who are  Filipino-Australian born and who do not speak the language, children of mixed marriages, scholars, diplomats, business people and individuals who are interested to learn the language for better understanding of the Filipino cultural heritage, identity and tradition.</p>
-        <p>The Victorian Curriculum and Assessment Authority has developed a curriculum for Roman Alphabet languages in which we, the Philippine Language School of Victoria uses.</p>
+        <p>The general objectives of the school are to teach and promote the use of Filipino Language for better understanding of the Filipino Cultural heritage, identity and tradition.</p>
+        <p>Specific Objective is:</p>
+           <ul>
+              <li>1. To teach non-Filipino speaking students to understand, speak, read, and write in Filipino.</li>
+              <li>2. To provide unique opportunity for students, teachers and parents to meet new friends, acquaintance, and to foster unity among Filipino Australian 
+                that is essential in a Multicultural society.</li>
+        <p>The first pilot Filipino classes commenced on 4 February 1995 at Maribyrnong Secondary College. These were run by volunteer teachers: Ruby Mamangun, Mertz Baera, 
+          Manuel Asuncion and Maria Violeta Binuya. After four years, the Department of Education granted the school as Single Study Provider and uses the guidelines in 
+          the Victorian Essential Learning Standards (VELS) provided by Victorian Curriculum Assessment Authority (VCAA) to attain specific objective. The school offered 
+          Filipino Units 1 &amp; 2 and Filipino Units 3 &amp; 4. On 28 February 2016 the PLSV was incorporated and granted a Certificate of Incorporation No. A0094860V. 
+          The school is based in Braybrook College, 352 Ballarat Rd. Braybrook, Victoria, 3019. The PLSV operate Filipino Language classes every Saturdays during the school term from 9AM to 12PM.</p>
+        <p>Current teaching and learning are aligned to the Victorian Curriculum F-10, Languages Version 2.0 in Non-Roman Alphabet Languages. They include activities in 
+          speaking, listening, reading, writing and viewing and reflect the intercultural dimension. As PLSV is an accredited VCE Filipino provider, we are using the VCE 
+          Filipino Study Design for VCE Filipino Units 1 and 2 and Units 3 and 4.</p>
+        <p>The Philippine Language School of Victoria teachers are qualified and accredited teachers in Australia. They have attended Certificate IV in Community Languages 
+          and other professional development training conducted by Community Languages Victoria. All teachers and volunteers have current Working with Children Check or VIT 
+          registration and have HLTAID009 Provide Cardiopulmonary Resuscitation yearly.</p>  
+        </ul>
         </div>
       </div>
   );
--- src/components/pages/Enrolment.js
+++ src/components/pages/Enrolment.js
@@ -10,37 +10,22 @@ export default function Enrolment() {
 people of all ages. Over 30 students (from Primary to
 Secondary and VCE level, as well as beginners of all ages including
 adults) have demonstrated their improved Filipino language skills from
-attending the Saturday morning classes at Braybrook or Wodonga on a weekly basis.
+attending the Saturday morning classes at Braybrook.
         </p>
-        <h2>Courses Offered:</h2>
-        <ul>
-              <li>&#8226; PRIMARY LEVEL FROM 6 YRS. TO 12 YRS. OLD</li>
-              <li>&#8226; SECONDARY LEVEL FROM 13 YRS. TO 18 YRS. OLD</li>
-              <li>&#8226; ADULT LEVEL FROM 18 YRS OLD AND ABOVE</li>
-        </ul>
-        <h2>We are VCE SINGLE PROVIDER approved by the Department of Education and Training</h2>
+        
         <p>Cost:</p>
         <ul>
-              <li>&#8226; VCE FILIPINO UNIT 1 & 2  :  $ 300</li>
-              <li>&#8226; VCE FILIPINO UNIT 3 & 4  :  $ 300</li>
-              <li>&#8226; Primary Level Fees :  $ 200</li>
-              <li>&#8226; Adult Class:  $ 400 (30 sessions)</li> 
+              <li>&#8226; VCE FILIPINO UNIT 1 to 4  :  $ 350</li>
+              <li>&#8226; Primary to Year 10 :  $300</li>
+              <li>&#8226; Adult Class:  $ 400 (35 sessions)</li> 
         </ul>
-        <h2>Term Dates 2022</h2>
-        <ul>
-              <li>&#8226; Term 1: 5 February - 9 April</li>
-              <li>&#8226; Term 2: 7 May - 25 June</li>
-              <li>&#8226; Term 3: 16 July - 1 October</li>
-              <li>&#8226; Term 4: 22 October - 17 December</li>
-        </ul>
-        <p>Day/Time: Saturday/9:00 - 12:00 noon</p>
         <h2>Venue</h2>
         <p>BRAYBROOK SECONDARY COLLEGE</p>
-        <p>352 BALLARAT RD. CORNER BURKE ST. BRAYBROOK</p>
+        <p>352 BALLARAT RD. BRAYBROOK (enter through Burke Street)</p>
         <h2>Email</h2>
-        <p>phillang@bigpond.net.au</p>
-        <h2>Contact Person</h2>
-        <p>Perla 0413 623 991</p>
+        <p>phillangsov@gmail.com</p>
+        <h2>Contact Number</h2>
+        <p>0412 836 883</p>
       </div>
     </div>
   );
