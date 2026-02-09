2401c12446bc7bd7ae6f12d114cf5e79059e9e5f
rayamigo18 <Amabelle Dormer>
2026-02-09 23:02:28 +1100

    code change

--- src/components/pages/Example.js
+++ src/components/pages/Example.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../../App.css";
-import Example from "./PLSV.pdf";
-import PdfViewer from "./components/PDFViewer";
+import PdfViewer from "../PDFViewer";
 
 
 export default function Example() {
